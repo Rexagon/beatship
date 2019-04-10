e9775442748f52ea05b707679434a2bdddced8cc
Rexagon <reide740@gmail.com>
2019-04-10 21:43:06 +0300

    Improved terrain audio waves

--- Assets/Scripts/AudioTerrain.cs
+++ Assets/Scripts/AudioTerrain.cs
@@ -5,26 +5,28 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class AudioTerrain : MonoBehaviour
 {
-    public AudioClip audioClip;
-    public Brick brickPrefab;
+    public AudioSource ship;
+
+    public float maxAttenuation = 40.0f;
+    public float maxDirectionAttenuation = 10.0f;
+    public float spectorFactor = 2.0f;
+    public float amplitude = 4.0f;
+    public float noiseScale = 0.1f;
 
     public int size = 256;
     public float step = 1.0f;
 
-    public float baseHeight = 5.0f;
-
     private float[] _spectrum = new float[256];
     private float[] _nextHeights;
-    private float[,] _heights;
 
     private Mesh _mesh;
 
     void Start()
     {
         _nextHeights = new float[size];
-        _heights = new float[size, size];
 
         _mesh = new Mesh();
+
         GetComponent<MeshFilter>().mesh = _mesh;
 
         CreateShape();
@@ -38,41 +40,45 @@ public class AudioTerrain : MonoBehaviour
         for (int i = 0; i < _spectrum.Length; ++i)
         {
             int index = Mathf.FloorToInt(Mathf.Log(i + 1) / maxX * (_nextHeights.Length - 1) * 0.5f);
-            _nextHeights[index] = _spectrum[i] * 10.0f;
+            _nextHeights[index] = _spectrum[i] * 10.0f / ship.volume;
             _nextHeights[size - index - 1] = _nextHeights[index];
         }
 
-        for (int j = 0; j < size; ++j)
-        {
-            if (j + 1 == size)
-            {
-                for (int i = 0; i < size; ++i)
-                {
-                    _heights[i, j] = (_heights[i, j] + _nextHeights[i] * Mathf.PerlinNoise(0.1f + i, 0.1f + j) * 2.0f) * 0.5f;
-                }
-            }
-            else
-            {
-                for (int i = 0; i < size; ++i)
-                {
-                    _heights[i, j] = _heights[i, j + 1];
-                    //_heights[i, j].offset = _nextHeights[i] * Mathf.PerlinNoise(0.1f + i, 0.1f + j) * 2.0f;
-                }
-            }
-        }
+        float sqrMaxAttenuation = maxAttenuation * maxAttenuation;
+        float sqrMaxDirectionAttenuation = maxDirectionAttenuation * maxDirectionAttenuation;
 
-        Vector3[] vertices = new Vector3[(size + 1) * (size + 1)];
+        Vector3 shipPosition = ship.transform.position;
+
+        Vector3[] vertices = _mesh.vertices;
 
         Vector3 origin = new Vector3(-1.0f, 0.0f, -1.0f) * (size + 1) * step * 0.5f;
-        for (int z = 0; z <= size; ++z)
+        for (int j = 0; j <= size; ++j)
         {
-            for (int x = 0; x <= size; ++x)
+            for (int i = 0; i <= size; ++i)
             {
-                vertices[z * (size + 1) + x] = origin + Vector3.right * step * x;
-                if (x < size && z < size)
+                float lastY = vertices[j * (size + 1) + i].y;
+
+                Vector3 postion = origin + Vector3.right * step * i;
+
+                Vector3 direction = shipPosition - postion - transform.position;
+
+                float sqrDistance = direction.sqrMagnitude;
+                float attenuation = Mathf.Max(sqrMaxAttenuation - sqrDistance, 0.0f) / sqrMaxAttenuation;
+
+                float sqrDirectionDistance = Vector3.Project(direction, ship.transform.right).sqrMagnitude;
+                float directionAttenuation = Mathf.Min(sqrDirectionDistance, sqrMaxDirectionAttenuation) / sqrMaxDirectionAttenuation + attenuation * attenuation;
+
+                int x = Mathf.FloorToInt(attenuation * size * spectorFactor / 2.0f);
+
+                if (x < size)
                 {
-                    vertices[z * (size + 1) + x] += Vector3.up * (_heights[x, z] + _nextHeights[x] * Mathf.PerlinNoise(0.1f + x, 0.1f + z) * 2.0f) * 0.5f;
+                    float noise = Mathf.PerlinNoise(noiseScale * (i + _nextHeights[x] * 10.0f), noiseScale * (j + _nextHeights[x] * 10.0f));
+
+                    postion.y += _nextHeights[x] * noise * amplitude * attenuation * directionAttenuation * 0.5f;
+                    postion.y += lastY * 0.5f;
                 }
+
+                vertices[j * (size + 1) + i] = postion;
             }
 
             origin += Vector3.forward * step;
